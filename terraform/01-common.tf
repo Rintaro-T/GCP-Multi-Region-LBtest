271e77388f101ea1846d2adeb0ec44eeda3f3ba2
Rintaro-T <rin22pq3_kk@keio.jp>
2021-09-20 02:42:49 +0900

    [write_change] TerraformコードのFW箇所をfor_eachで書き直した

--- terraform/01-common.tf
+++ terraform/01-common.tf
@@ -1,6 +1,24 @@
 
-#プロジェクト名の指定
 locals {
+  #プロジェクト名の指定
   project = "multi-region-lbtest-20210917"
+
+
+  #FWルールのfor_each
+  fw-rules = {
+
+    #httpファイヤウォールの設定
+    mrlb-http = {
+      protocol = "tcp"
+      ports    = ["80"]
+    }
+
+    #icmpファイヤウォールの設定
+    mrlb-icmp = {
+      protocol = "icmp"
+      ports    = []
+    }
+  }
+
 }
 
