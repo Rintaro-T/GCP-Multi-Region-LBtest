89aac855841452db89efc34582c7a44fdb636afb
Rintaro-T <rin22pq3_kk@keio.jp>
2021-09-22 18:07:58 +0900

    [write_del] Terraformコード内にてregionとzonesに関するvariableを使わなくなったためコードから削除

--- terraform/00-variables.tf
+++ terraform/00-variables.tf
@@ -1,34 +1,13 @@
-
+#base_name ------------------------------------------------
 variable "base_name" {
   type    = string
   default = "mrlb"
 }
 
+#instance_type ------------------------------------------
 variable "instance_type" {
   type    = string
   default = "f1-micro"
-}
-
-#regon-----------------------------------------------------
-variable "instance_region" {
-  type    = string
-  default = "asia-northeast1"
-}
-
-variable "instance_region2" {
-  type    = string
-  default = "us-west4"
-}
-
-#region1 zones---------------------------------------------
-variable "instance_zones" {
-  type    = list(string)
-  default = ["asia-northeast1-a", "asia-northeast1-b", "asia-northeast1-c"]
-}
-
-variable "instance_zones2" {
-  type    = list(string)
-  default = ["us-west4-a", "us-west4-b", "us-west4-c"]
 }
 
 #Service Account ------------------------------------------
@@ -37,7 +16,7 @@ variable "sa_name" {
   default = "mrlb-vm"
 }
 
-#Instance Group Auto Scalerの閾値のCPU使用率
+#Instance Group Auto Scalerの閾値のCPU使用率---------------
 variable "cpu_utilization_target" {
   type    = number
   default = 0.4
